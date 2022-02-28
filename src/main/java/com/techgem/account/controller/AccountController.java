4d789ece769b342688fe0bc45eb24123c57454b4
Sandipan Sarkar <work.sandipan@gmail.com>
2022-02-28 19:46:45 +0530

    https://techgem1986-jira.atlassian.net/browse/TG001-3

--- src/main/java/com/techgem/account/controller/AccountController.java
+++ src/main/java/com/techgem/account/controller/AccountController.java
@@ -1,16 +1,18 @@
-package com.techgem.account.controllers;
+package com.techgem.account.controller;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(value = "/account")
 @RestController
+@Api(value="AccountController")
 public class AccountController {
 
     @GetMapping("/health-check")
     public String healthCheck(){
-        return "Success";
+        return "Successfully Called!";
     }
 
 }
